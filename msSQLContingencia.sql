419345ee6862814da12defaa2db8033b2ba9774b
PruebaGit <n.abuin2005@gmail.com>
2025-01-26 13:44:14 +0100

    Actualizacion del plan de contingencia para Peliculas

--- msSQLContingencia.sql
+++ msSQLContingencia.sql
@@ -36,3 +36,31 @@ VALUES ('Sala 1', 100);
 
 INSERT INTO Salas (nombre, capacidad)
 VALUES ('Sala 2', 100);
+
+
+-- Creacion de tabla Peliculas y Peliculas
+
+CREATE TABLE Peliculas (
+    idPelicula INT PRIMARY KEY IDENTITY(1,1),
+    titulo VARCHAR(50) NOT NULL,
+    sinopsis VARCHAR(50),
+    duracion DECIMAL(4,3),
+    categoria VARCHAR(50),
+    director VARCHAR(50),
+    anio DATETIME,
+    imagenURL VARCHAR(50),
+    puntuacion INT
+);
+
+
+INSERT INTO Peliculas (titulo, sinopsis, duracion, categoria, director, anio, imagenURL, puntuacion)
+VALUES 
+('Inception', 'Un ladrón roba secretos a través de sueños.', 2.148, 'Ciencia ficción', 'Christopher Nolan', '2010-07-16', 'https://imagen.com/inception.jpg', 9);
+
+INSERT INTO Peliculas (titulo, sinopsis, duracion, categoria, director, anio, imagenURL, puntuacion)
+VALUES 
+('The Godfather', 'La historia de una familia mafiosa.', 2.555, 'Drama', 'Francis Ford Coppola', '1972-03-24', 'https://imagen.com/godfather.jpg', 10);
+
+INSERT INTO Peliculas (titulo, sinopsis, duracion, categoria, director, anio, imagenURL, puntuacion)
+VALUES 
+('Toy Story', 'Un juguete lidera una aventura para volver a casa.', 1.400, 'Animación', 'John Lasseter', '1995-11-22', 'https://imagen.com/toystory.jpg', 8);
