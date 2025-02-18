25898d7ec5c69dbb520f79631647f1357ce3e224
PruebaGit <n.abuin2005@gmail.com>
2025-02-18 18:10:04 +0100

    Creacion de CORS

--- Program.cs
+++ Program.cs
@@ -6,27 +6,30 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("cinema");
 
 builder.Services.AddScoped<IUsuariosRepository, UsuariosRepository>(provider =>
-new UsuariosRepository(connectionString));
-
+    new UsuariosRepository(connectionString));
 
 builder.Services.AddScoped<ISalasRepository, SalasRepository>(provider =>
-new SalasRepository(connectionString));
-
+    new SalasRepository(connectionString));
 
 // Add services to the container.
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Configurar CORS para permitir cualquier origen
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
+// Configurar Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//Add services
+// Add services
 builder.Services.AddScoped<IUsuariosService, UsuariosService>();
-
-
 builder.Services.AddScoped<ISalasService, SalasService>();
-
-
 
 var app = builder.Build();
 
@@ -39,11 +42,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Aplicar CORS a toda la API
+app.UseCors("AllowAll");
+
 app.UseAuthorization();
 
 app.MapControllers();
 
-
-
-//PlatoPrincipalController.InicializarDatos();
+// PlatoPrincipalController.InicializarDatos();
 app.Run();
