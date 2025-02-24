f7db6d4f221ba0acb6ed8a85694c704392846a09
PruebaGit <n.abuin2005@gmail.com>
2025-02-24 17:27:20 +0100

    Conversion de tipos de algunos datos en Salas

--- Models/Salas.cs
+++ Models/Salas.cs
@@ -7,17 +7,18 @@ public class Salas{
 
     public int IdSala{get; set;}
     public string  Nombre  {get; set;}
-    public string  Capacidad {get; set;}
+    public int  Capacidad {get; set;}
     public int  IdTipoSala {get; set;}
     public int  IdSedes {get; set;}
     public Salas(){} // CONTRUCTOR VACIO INYECCION DE DEPENDENCIAS
 
-    public Salas(int idSala, string nombre, string capacidad, int idTipoSala, int idTipoSede){
+    public Salas(int idSala, string nombre, int capacidad, int idTipoSala, int idSedes){
 
         IdSala = idSala;
         Nombre = nombre;
         Capacidad = capacidad;
-        IdTipoSala = idTipoSede;
+        IdTipoSala = idTipoSala;
+        IdSedes = idSedes;
     }
 
 
