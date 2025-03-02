c5c4c4b1c455774870d449c45f450fdc7140829a
PruebaGit <n.abuin2005@gmail.com>
2025-03-02 21:47:12 +0100

    Aplicar automaticamente el precio del asiento en la linea

--- Repository/LineasRepository.cs
+++ Repository/LineasRepository.cs
@@ -21,7 +21,7 @@ namespace CoWorking.Repositories
             {
                 await connection.OpenAsync();
 
-                string query = "SELECT IdLinea, IdReserva, IdDetallerReserva, Precio FROM Lineas";
+                string query = "SELECT IdLinea, IdReserva, IdDetalleReserva, Precio FROM Lineas";
                 using (var command = new SqlCommand(query, connection))
                 {
                     using (var reader = await command.ExecuteReaderAsync())
@@ -82,13 +82,64 @@ namespace CoWorking.Repositories
             {
                 await connection.OpenAsync();
 
-                string query = "INSERT INTO Lineas (IdReserva, IdDetallerReserva, Precio) VALUES (@IdReserva, @IdDetalleReserva, @Precio)";
+                await connection.OpenAsync();
 
-                using (var command = new SqlCommand(query, connection))
+                // Consultas SQL para poder conseguir los ids
+                string queryIdPuestoTrabajo = "SELECT IdPuestoTrabajo FROM DetallesReservas WHERE IdDetalleReserva = @IdDetalleReserva";
+                string queryIdSala = "SELECT IdSala FROM PuestosTrabajo WHERE IdPuestoTrabajo = @IdPuestosTrabajoQuery";
+                string queryIdTipoSala = "SELECT IdTipoSala FROM Salas WHERE IdSala = @IdSalaQuery";
+                string queryIdTipoPuestoTrabajo = "SELECT IdTipoPuestoTrabajo FROM TiposSalas WHERE IdTipoSala = @IdTipoSalaQuery";
+                string queryPrecio = "SELECT Precio FROM TiposPuestosTrabajo WHERE IdTipoPuestoTrabajo = @queryIdTipoPuestoTrabajoQuery";
+
+                // Obtener valores de la base de datos
+                int idPuestoTrabajo;
+                using (var command = new SqlCommand(queryIdPuestoTrabajo, connection))
+                {
+                    command.Parameters.AddWithValue("@IdDetalleReserva", linea.IdDetalleReserva);
+                    var result = await command.ExecuteScalarAsync();
+                    idPuestoTrabajo = result != null ? Convert.ToInt32(result) : 0;
+                }
+
+                int idSala;
+                using (var command = new SqlCommand(queryIdSala, connection))
+                {
+                    command.Parameters.AddWithValue("@IdPuestosTrabajoQuery", idPuestoTrabajo);
+                    var result = await command.ExecuteScalarAsync();
+                    idSala = result != null ? Convert.ToInt32(result) : 0;
+                }
+
+                int idTipoSala;
+                using (var command = new SqlCommand(queryIdTipoSala, connection))
+                {
+                    command.Parameters.AddWithValue("@IdSalaQuery", idSala);
+                    var result = await command.ExecuteScalarAsync();
+                    idTipoSala = result != null ? Convert.ToInt32(result) : 0;
+                }
+
+                int idTipoPuestoTrabajo;
+                using (var command = new SqlCommand(queryIdTipoPuestoTrabajo, connection))
+                {
+                    command.Parameters.AddWithValue("@IdTipoSalaQuery", idTipoSala);
+                    var result = await command.ExecuteScalarAsync();
+                    idTipoPuestoTrabajo = result != null ? Convert.ToInt32(result) : 0;
+                }
+
+                decimal precio;
+                using (var command = new SqlCommand(queryPrecio, connection))
+                {
+                    command.Parameters.AddWithValue("@queryIdTipoPuestoTrabajoQuery", idTipoPuestoTrabajo);
+                    var result = await command.ExecuteScalarAsync();
+                    precio = result != null ? Convert.ToDecimal(result) : 0m;
+                }
+
+                // Insertar en la tabla Lineas con los valores obtenidos
+                string queryInsert = "INSERT INTO Lineas (IdReserva, IdDetalleReserva, Precio) VALUES (@IdReserva, @IdDetalleReserva, @Precio)";
+                using (var command = new SqlCommand(queryInsert, connection))
                 {
                     command.Parameters.AddWithValue("@IdReserva", linea.IdReserva);
                     command.Parameters.AddWithValue("@IdDetalleReserva", linea.IdDetalleReserva);
-                    command.Parameters.AddWithValue("@Precio", linea.Precio);
+                    command.Parameters.AddWithValue("@Precio", precio);
+
                     await command.ExecuteNonQueryAsync();
                 }
             }
