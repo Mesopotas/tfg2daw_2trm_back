49f6e7d079368ee20921111221e2da11d492bed9
PruebaGit <n.abuin2005@gmail.com>
2025-03-02 12:38:58 +0100

    Creacion de Controller de Reservas

---
+++ Controllers/ReservasController.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Mvc;
+using CoWorking.Repositories;
+using CoWorking.Service;
+using CoWorking.DTO;
+using Models;
+
+namespace CoWorking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservasController : ControllerBase
+    {
+        private static List<Reservas> detalleReservas = new List<Reservas>();
+
+        private readonly IReservasService _serviceReservas;
+
+        public ReservasController(IReservasService service)
+        {
+            _serviceReservas = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Reservas>>> GetReservas()
+        {
+            var reservas = await _serviceReservas.GetAllAsync();
+            return Ok(reservas);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Reservas>> GetReserva(int id)
+        {
+            var detalleReservas = await _serviceReservas.GetByIdAsync(id);
+            if (detalleReservas == null)
+            {
+                return NotFound();
+            }
+            return Ok(detalleReservas);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Reservas>> CreateReserva(Reservas reservas)
+        {
+            await _serviceReservas.AddAsync(reservas);
+            return CreatedAtAction(nameof(CreateReserva), new { id = reservas.IdReserva }, reservas);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateReserva(int id, Reservas updatedReservas)
+        {
+            var existingReserva = await _serviceReservas.GetByIdAsync(id);
+            if (existingReserva == null)
+            {
+                return NotFound();
+            }
+            existingReserva.Descripcion = updatedReservas.Descripcion;
+
+
+            await _serviceReservas.UpdateAsync(existingReserva);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReserva(int id)
+        {
+            var detalleReservas = await _serviceReservas.GetByIdAsync(id);
+            if (detalleReservas == null)
+            {
+                return NotFound();
+            }
+            await _serviceReservas.DeleteAsync(id);
+            return NoContent();
+        }
+
+    }
+}
